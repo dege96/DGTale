29e30f944c47c6bc49f52a604fc85ef7c6f23d2e
dege96 <victordegeer96@gmail.com>
2025-02-06 18:45:23 +0100

    Initial commit

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from '/src/components/Header';
 import Hero from '/src/components/Hero';
-import Section1 from '/src/components/Section1';
-import Section2 from '/src/components/Section2';
-import Section3 from '/src/components/Section3';
-import Section4 from '/src/components/Section4';
-import Section5 from '/src/components/Section5';
-import Section6 from '/src/components/Section6';
-import Section7 from '/src/components/Section7';
+import AboutSection from '/src/components/AboutSection';
+import ServicesSection from '/src/components/ServicesSection';
+import PortfolioSection from '/src/components/PortfolioSection';
+import TestimonialsSection from '/src/components/TestimonialsSection';
+import PricingSection from '/src/components/PricingSection';
+import BlogSection from '/src/components/BlogSection';
+import ContactSection from '/src/components/ContactSection';
 import Footer from '/src/components/Footer';
 
 function App() {
@@ -16,13 +16,13 @@ function App() {
     <div className="App">
       <Header />
       <Hero />
-      <Section1 />
-      <Section2 />
-      <Section3 />
-      <Section4 />
-      <Section5 />
-      <Section6 />
-      <Section7 />
+      <AboutSection />
+      <ServicesSection />
+      <PortfolioSection />
+      <TestimonialsSection />
+      <PricingSection />
+      <BlogSection />
+      <ContactSection />
       <Footer />
     </div>
   );
