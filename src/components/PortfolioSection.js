c1680dcdf8b52c1d17beb465fb67d5964ce0c973
dege96 <victordegeer96@gmail.com>
2025-04-30 00:18:03 +0200

    din commit message

--- src/components/PortfolioSection.js
+++ src/components/PortfolioSection.js
@@ -8,21 +8,21 @@ const PortfolioSection = () => {
         <h2>Kundcase</h2>
         <div className="portfolio-grid">
           <div className="portfolio-item">
-            <img src="/images/hasams.png" alt="Projekt 1" />
+            <img src="/images/hasams.png" alt="Hasams AB - Redovisningsbyrå i Tyresö" />
             <div className="portfolio-overlay" onClick={() => window.open('https://hasamsredovisning.se', '_blank')}>
               <h3>HaSams AB</h3>
               <p>Redovisningsbyrå i Tyresö</p>
             </div>
           </div>
           <div className="portfolio-item">
-            <img src="/images/dgd.png" alt="Projekt 2" />
+            <img src="/images/dgd.png" alt="DGD Solutions - Konsultation inom teknisk projektledning, design och tillverkning." />
             <div className="portfolio-overlay" onClick={() => window.open('https://dg-development.vercel.app/', '_blank')}>
               <h3>DGD Solutions</h3>
               <p>Konsultation inom teknisk projektledning, design och tillverkning.</p>
             </div>
           </div>
           <div className="portfolio-item">
-            <img src="/images/brown.png" alt="Projekt 3" />
+            <img src="/images/brown.png" alt="Brown VVS - VVS-företag i Saltsjöbaden" />
             <div className="portfolio-overlay" onClick={() => window.open('https://brownvvs-v2-1.vercel.app/', '_blank')}>
               <h3>Brown VVS</h3>
               <p>VVS-företag i Saltsjöbaden</p>
